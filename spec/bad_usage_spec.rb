8cd8b674f9e80e8e7fd83c28ae31d144297fb4e3
jnieto1974 <jose.nieto@talan.com>
2018-11-16 06:46:39 +0100

    prueba

--- spec/bad_usage_spec.rb
+++ spec/bad_usage_spec.rb
@@ -14,8 +14,8 @@ describe 'Bad usage' do
     i_handle_everything_except_the_grounds
     # When I take "50" coffees
     i_take_coffee_number_coffees(50)
-    # Then message "Empty grounds" should be displayed
-    message_message_should_be_displayed("Empty grounds ")
+    # Then message "Empty grounds" should be displayed 
+    message_message_should_be_displayed("Empty grounds 3 ")
     # And coffee should be served
     coffee_should_be_served
   end
